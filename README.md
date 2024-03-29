418dc98ece785ec7485c978971b3bb06bd513c70
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-03-29 11:27:36 +0700

    notif package status

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 ﻿# ssg_unconstraint
+
+## status
+
+-   not yet stable
+-   wait for few days or until further note
+
+## how to install
+
+```shell
+node i @html_first/ssg_unconstraint
+node ./node_modules/@html_first/ssg_unconstraint/starter.mjs
+
+```
---
+++ starter.mjs
@@ -0,0 +1,20 @@
+﻿// @ts-check
+import ncp from 'ncp';
+import path from 'path';
+
+const base = process.cwd();
+const folder_ = 'h_client_example';
+const source_dir = path.join(base, 'node_modules', '@html_first', 'html_scrambler', folder_);
+const dest_dir = path.join(base, folder_);
+
+const options = {
+	clobber: true,
+};
+[source_dir].forEach((folder__) => {
+	ncp.ncp(folder__, dest_dir, options, function (err) {
+		if (err) {
+			return console.error(err);
+		}
+		console.log('example files copied successfully!');
+	});
+});
