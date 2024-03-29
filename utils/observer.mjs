27266af17fd5a2184db9310d928c9a91efdf7984
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-03-29 08:17:58 +0700

    create observer

---
+++ utils/observer.mjs
@@ -0,0 +1,32 @@
+﻿// @ts-check
+
+export class observer {
+	constructor() {}
+	/** @private */
+	config = { attributes: true, childList: true, subtree: true };
+	run = () => {
+		const observer_ = new MutationObserver(this.watcher);
+		const targetNode = document.querySelector(this.target);
+		if (!targetNode) {
+			return;
+		}
+		observer_.observe(targetNode, this.config);
+	};
+	/** @private */
+	target = 'body';
+	/**
+	 * @private
+	 * @param {MutationRecord[]} mutations_list
+	 * @param {MutationObserver} observer
+	 */
+	watcher = (mutations_list, observer) => {
+		mutations_list.forEach((mutation) => {
+			console.log(mutation);
+			if (mutation.type === 'attributes') {
+				console.log('Attributes mutated');
+			} else if (mutation.type === 'childList') {
+				console.log('Child list mutated');
+			}
+		});
+	};
+}
