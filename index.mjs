9bb17defdfd99ab767e764eb8640cad0fc36d2e1
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-18 21:21:29 +0700

    patch

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
 ﻿// @ts-check
 
 import { _ClientExtender } from './src/utils/_CientExtender.mjs';
+import { __ClassList } from './src/utils/__ClassList.mjs';
 import { __Observer } from './src/utils/__Observer.mjs';
 import { __AppSettings } from './src/vars/__AppSettings.mjs';
 
-export { __Observer, __AppSettings, _ClientExtender };
+export { __Observer, __AppSettings, _ClientExtender, __ClassList };
