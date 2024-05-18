6dea17f0277244f315aa7cc4b1f437e0dcef43e2
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-18 21:25:46 +0700

    ok

--- client/html_first/ClassList.mjs
+++ client/html_first/ClassList.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { __ClassList } from '../../src/utils/__ClassList.mjs';
+import { __ClassList } from '@html_first/ssg_unconstraint';
 import { alpine } from './classes/alpine.mjs';
 import { bss } from './classes/bss.mjs';
 import { html } from './classes/html.mjs';
